82c3ba7acf3a1ac4e50e004ca86325ecca10661d
Arjunsingh003 <get2arjunsingh@gmail.com>
2023-06-14 16:23:25 +0530

    made some changes

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
  const app = require('./app');
  const cors = require('cors');
- const dotenv = require('dotenv');
+ const jwt = require('express-jwt');
+ const jwks = require('jwks-rsa');
+ const axios = require('axios');
+
+//  const dotenv = require('dotenv');
 
 
 
 // Create a web server with express instance
 const server = express();
-const PORT = process.env.PORT || 4000
+
 
 /**
  * Configuration of ENV Variables
@@ -24,8 +28,53 @@ const db = require("./dbconfig");
  * Import and register the Express application controller
 */
 server.use(cors());
+
+const verifyJwt = jwt({
+    secret:jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://yoursincerely.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://backend-cap.onrender.com/colors',
+    issuer: 'https://yoursincerely.us.auth0.com/',
+    algorithms: ['RS256']
+}).unless({path: ['/']});
+server.use(verifyJwt);
 server.use("/", app);
 
+app.get('/colors', async (req, res) => {
+    try {
+      const accessToken = req.headers.authorization.split(' ')[1];
+      const response = await axios.get('https://yoursincerely.us.auth0.com/userinfo', {
+        headers: {
+          authorization: `Bearer ${accessToken}`
+        }
+      });
+      const userInfo = response.data;
+      console.log(userInfo);
+      res.send(userInfo);
+    } catch (error) {
+      res.send(error.message);
+    }
+  });
+  
+
+
+server.use((req,res, next )=>{
+    const error =  new Error('Notfound');
+    error.status = 404;
+    next(error);
+    });
+
+server.use((error, req, res, next) =>{
+    const status = error.status || 500
+    const message =  error.message || 'Internal Server Error'
+    res.status(status).send(message);
+}
+);
+
+const PORT = process.env.PORT || 8000
 
 
 
